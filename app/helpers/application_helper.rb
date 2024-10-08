1dcf3a320e870cb9a50e8f59ee3e988f46f96ef7
Jorge <jorgeluiscardoza89@gmail.com>
2024-10-08 18:15:01 +0300

    commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -42,4 +42,19 @@ module ApplicationHelper
     end
   end
 
+  def class_for_boolean(flag)
+    if flag
+      return "bg-green-100 text-green-800 text-xs font-medium border border-green-300 me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300"
+    else
+      return "bg-red-100 text-red-800 text-xs font-medium border border-red-300 me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300"
+    end
+  end
+
+  def text_for_boolean(flag)
+    if flag
+      "Yes"
+    else
+      "No"
+    end
+  end
 end
