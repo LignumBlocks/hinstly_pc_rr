686aeca655bddf0eb060c85b7b645e01b229c392
Jorge <jorgeluiscardoza89@gmail.com>
2024-10-15 10:30:44 +0300

    Cmmit

--- app/models/services/scrapper.rb
+++ app/models/services/scrapper.rb
@@ -2,10 +2,11 @@ require 'capybara'
 require 'capybara/dsl'
 require 'selenium-webdriver'
 
+Capybara.default_driver = :selenium_chrome
+
 module Services
   class Scrapper
     include Capybara::DSL
-    Selenium::WebDriver::Chrome::Service.driver_path = '/usr/bin/chromedriver'
 
     def initialize(sources, queries)
       options = Selenium::WebDriver::Chrome::Options.new
