dc349e02ce9b4e8574a1b1b6faae5ce7e2ec625f
NileyGF <nileygf@gmail.com>
2024-10-23 20:15:01 -0400

    Check for null, empty string, or too short content in extract_hack_from_video

--- app/models/ai/video.rb
+++ app/models/ai/video.rb
@@ -6,13 +6,26 @@ module Ai
 
     def find_hack!
       hack = extract_hack_from_video
-      @video.create_hack(title: hack['possible hack title'], summary: hack['brief summary'], justification: hack['justification'], is_hack: hack['is_a_hack'])
+      @video.create_hack(title: hack['possible hack title'], summary: hack['brief summary'],
+                         justification: hack['justification'], is_hack: hack['is_a_hack'])
       self
     end
 
     private
 
     def extract_hack_from_video
+      # Check for null, empty string, or too short content (less than 50 words)
+      if @video.transcription&.content.nil? ||
+         @video.transcription&.content&.empty? ||
+         (@video.transcription&.content&.split&.size&.<= 50)
+        return {
+          "possible hack title": 'None',
+          "brief summary": 'None',
+          "justification": 'Empty or too short source text.',
+          "is_a_hack": false
+        }
+      end
+      # Proceed with the original logic if the content is valid
       prompt = Prompt.find_by_code('HACK_DISCRIMINATION_REDUCED')
       prompt_text = prompt.build_prompt_text({ source_text: @video.transcription&.content })
       system_prompt_text = prompt.system_prompt
