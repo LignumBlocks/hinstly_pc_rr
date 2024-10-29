4621f6132e7ac97b73d3b440c3d2466df7b11bcd
Jorge <jorgeluiscardoza89@gmail.com>
2024-10-29 22:43:53 +0300

    Update scrapper.rb

--- app/models/services/scrapper.rb
+++ app/models/services/scrapper.rb
@@ -11,7 +11,7 @@ module Services
       @options.add_argument('--disable-gpu')
       @options.add_argument('--no-sandbox')
 
-      @driver = Selenium::WebDriver.for :chrome, options: options
+      @driver = Selenium::WebDriver.for :chrome, options: @options
       @sources = sources
       @queries = queries
     end
