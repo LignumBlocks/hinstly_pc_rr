b7497f8f7d3132cf1ca2867862936fa810e346ea
Jorge <jorgeluiscardoza89@gmail.com>
2024-10-29 22:37:45 +0300

    Update scrapper.rb

--- app/models/services/scrapper.rb
+++ app/models/services/scrapper.rb
@@ -6,10 +6,10 @@ module Services
     include Capybara::DSL
 
     def initialize(sources, queries)
-      options = Selenium::WebDriver::Chrome::Options.new
-      options.add_argument('--headless')
-      options.add_argument('--disable-gpu')
-      options.add_argument('--no-sandbox')
+      @options = Selenium::WebDriver::Chrome::Options.new
+      @options.add_argument('--headless')
+      @options.add_argument('--disable-gpu')
+      @options.add_argument('--no-sandbox')
 
       @driver = Selenium::WebDriver.for :chrome, options: options
       @sources = sources
@@ -39,7 +39,7 @@ module Services
           end
         end
         @driver.quit
-        @driver = Selenium::WebDriver.for :chrome, options: options
+        @driver = Selenium::WebDriver.for :chrome, options: @options
       end
       @driver.quit
     end
