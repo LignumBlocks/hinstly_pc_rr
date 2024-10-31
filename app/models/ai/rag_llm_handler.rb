f12ca5f2f829a8442030bf48c72326d1563c811f
NileyGF <nileygf@gmail.com>
2024-10-31 15:26:52 -0400

    store_from_queries does not receives a hash anymore

--- app/models/ai/rag_llm_handler.rb
+++ app/models/ai/rag_llm_handler.rb
@@ -2,7 +2,7 @@ module Ai
   # A class for managing a Retrieval-Augmented Generation (RAG) model using LLMs and a Chroma vector store.
   # This class allows the addition and retrieval of documents from a vector store for better context-aware conversations.
   class RagLlmHandler < BaseHandler
-    attr_reader :model_name, :llm,:collection_name
+    attr_reader :model_name, :llm, :collection_name
 
     def initialize(model_name = 'gemini-1.5-flash-8b', temperature = 0.4, collection_name = 'validation')
       super(model_name, temperature)
@@ -11,8 +11,8 @@ module Ai
     end
 
     def validation_for_hack(hack)
-      store_from_queries(validation_sources_hashes_list(hack), hack.id)
-      validation_retrieval_generation(hack.id, hack.title, hack.summary)
+      store_from_queries(hack)
+      validation_retrieval_generation(hack)
     end
 
     def create_or_load_vs
@@ -46,33 +46,36 @@ module Ai
     end
 
     # Stores documents from a list of query results into the Chroma vector store, splitting them into chunks.
-    def store_from_queries(queries_dict, hack_id)
-      queries_dict.each do |query_dict|
-        next unless query_dict[:content]
+    def store_from_queries(hack)
+      hack.queries.each do |query|
+        query.scraped_results.each do |scraped_result|
+          next unless scraped_result.content
 
-        content_chunks = Langchain::Chunker::RecursiveText.new(query_dict[:content], chunk_size: 2000,
-                                                                                     chunk_overlap: 300).chunks
-        content_chunks.each do |chunk|
-          metadata = { "hack_id": hack_id.to_s, "query": query_dict[:query],
-                       "link": query_dict[:link], "content": chunk.text }
-          add_document([chunk.text], metadata)
+          content_chunks = Langchain::Chunker::RecursiveText.new(scraped_result.content, chunk_size: 2000,
+                                                                                         chunk_overlap: 300).chunks
+          content_chunks.each do |chunk|
+            metadata = { "hack_id": hack.id.to_s, "query": query.content, "link": scraped_result.link,
+                         "content": chunk.text }
+            add_document([chunk.text], metadata)
+          end
         end
       end
     end
 
-    def validation_retrieval_generation(hack_id, hack_title, hack_summary)
+    def validation_retrieval_generation(hack)
       model = Ai::LlmHandler.new('gemini-1.5-flash-8b')
       chunks = ''
       links = []
       # TODO: design a clustering method to select more sources and then make iterations in the validation process or a maxing of the results
-      similar_chunks = retrieve_similar_for_hack(@collection_name, "#{hack_title}:\n#{hack_summary}",
-                                                 { "hack_id": hack_id.to_s })
+      similar_chunks = retrieve_similar_for_hack(@collection_name, "#{hack.title}:\n#{hack.summary}",
+                                                 { "hack_id": hack.id.to_s })
       similar_chunks.each do |result|
         links << result['metadata']['link']
         chunks += "Relevant context section:\n#{result['metadata']['content']}\n\n"
       end
       prompt = Prompt.find_by_code('HACK_VALIDATION')
-      prompt_text = prompt.build_prompt_text({ chunks: chunks.strip, hack_title:, hack_summary: })
+      prompt_text = prompt.build_prompt_text({ chunks: chunks.strip, hack_title: hack.title,
+                                               hack_summary: hack.summary })
       system_prompt_text = prompt.system_prompt
       result = model.run(prompt_text, system_prompt_text)
       result = JSON.parse(result.gsub("```json\n", '').gsub('```', '').strip)
@@ -83,12 +86,14 @@ module Ai
       }
     end
 
+    # deprecated
     def get_clean_links(metadata)
       links = metadata.map { |item| item[0] }
       unique_links = links.uniq
       unique_links.join(' ')
     end
 
+    # deprecated
     def validation_sources_hashes_list(hack)
       hashes = []
       hack.queries.each do |query|
