b0e758e8a53c595c443c5cc7de1d1c1bce79750b
rrnoa <roiky82@gmail.com>
2024-11-03 11:29:02 -0500

    10 jobs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,6 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379/0' }
+  config.options[:concurrency] = ENV.fetch('SIDEKIQ_CONCURRENCY', 10).to_i
 end
 
 Sidekiq.configure_client do |config|
